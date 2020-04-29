5b6612cb8c33ccfc7edaf62f9b4588a903775198
천준수 <JUNSU@cheonjunsuui-MacBook-Air.local>
2020-04-29 14:09:11 +0900

    Updates

---
+++ precache-manifest.e808c28283c832c314d527282e1b04f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf09b18d7e46b8983f24d5acc649c8d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1df27f523a23fa51186d",
+    "url": "/movie_app_2020/static/css/main.06dbccec.chunk.css"
+  },
+  {
+    "revision": "f0fd331f2398bdaee033",
+    "url": "/movie_app_2020/static/js/2.20c7e5f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.20c7e5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df27f523a23fa51186d",
+    "url": "/movie_app_2020/static/js/main.f8081012.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
